bfc829f740dd4c1456cd60ed8c4cf6cd4be3ed77
Oscar Santos <oscarsantos.go@gmail.com>
2024-01-11 08:59:44 -0800

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Oscar Santos on 1/11/24.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
