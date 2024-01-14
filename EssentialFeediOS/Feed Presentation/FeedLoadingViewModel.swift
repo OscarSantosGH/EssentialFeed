10735f300210fa7908a8bd3291d603acde2443ed
Oscar Santos <oscarsantos.go@gmail.com>
2024-01-13 20:04:49 -0800

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Oscar Santos on 1/13/24.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Oscar Santos on 1/13/24.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
