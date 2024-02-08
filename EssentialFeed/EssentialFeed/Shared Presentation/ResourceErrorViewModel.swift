b0952a992416aec41a9fca0ee2dfefc0574db4f4
Oscar Santos <oscarsantos.go@gmail.com>
2024-02-08 10:46:38 -0800

    Introduce reusable `LoadResourcePresenter`.

---
+++ EssentialFeed/EssentialFeed/Shared Presentation/ResourceErrorViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ResourceErrorViewModel.swift
+//  EssentialFeed
+//
+//  Created by Oscar Santos on 2/8/24.
+//
+
+import Foundation
+
+public struct ResourceErrorViewModel {
+    public let message: String?
+    
+    static var noError: ResourceErrorViewModel {
+        return ResourceErrorViewModel(message: nil)
+    }
+    
+    static func error(message: String) -> ResourceErrorViewModel {
+        return ResourceErrorViewModel(message: message)
+    }
+}
---
+++ EssentialFeed/EssentialFeed/Shared Presentation/ResourceLoadingView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceLoadingView.swift
+//  EssentialFeed
+//
+//  Created by Oscar Santos on 2/8/24.
+//
+
+import Foundation
+
+public protocol ResourceLoadingView {
+    func display(_ viewModel: ResourceLoadingViewModel)
+}
